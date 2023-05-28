a5196f83b495e8204e94d76adeb5b53d0e5a0ea5
kimlee <kiim.n.lee@gmail.com>
2023-05-28 13:58:49 -0500

    updated User fields and unit test

--- Server/src/test/java/women/whocode/todoapp/TodoappApplicationTests.java
+++ Server/src/test/java/women/whocode/todoapp/TodoappApplicationTests.java
@@ -18,15 +18,17 @@ class TodoappApplicationTests {
 
 	@Test
 	void testUserConstructor(){
-		User testUser = new User("testName", "testPassword");
+		User testUser = new User("testName", "testPassword", "testEmail", "testPhone");
 		assertEquals(0, testUser.getId());
 		assertEquals("testName", testUser.getUsername());
 		assertEquals("testPassword", testUser.getPassword());
+		assertEquals("testEmail", testUser.getEmail());
+		assertEquals("testPhone", testUser.getPhoneNumber());
 	}
 
 	@Test
 	void testTaskConstructor(){
-		User testUser = new User("testName", "testPassword");
+		User testUser = new User("testName", "testPassword", "testEmail", "testPhone");
 		Task testTask = new Task(testUser, "wash clothes", 2, "cleaning", "don't machine dry sweater");
 		assertEquals("testName", testUser.getUsername());
 		assertEquals("wash clothes", testTask.getActivity());
